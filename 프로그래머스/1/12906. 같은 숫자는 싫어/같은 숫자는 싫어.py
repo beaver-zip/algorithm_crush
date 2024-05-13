b4fe40470893b699149340145ffd2128c94706a1
beaver.zip <95961223+beaver-zip@users.noreply.github.com>
2024-05-14 01:36:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 133.77 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,6 +1,13 @@
+# def solution(arr):
+#     new = []
+#     for i in range(1, len(arr)):
+#         if arr[i-1] != arr[i]:
+#             new.append(arr[i-1])
+#     return new + [arr[-1]]
+
 def solution(arr):
-    new = [arr[0]]
-    for i in range(1, len(arr)):
-        if arr[i-1] != arr[i]:
-            new.append(arr[i])
+    new = []
+    for i in arr:
+        if new[-1:] == [i]: continue
+        new.append(i)
     return new
