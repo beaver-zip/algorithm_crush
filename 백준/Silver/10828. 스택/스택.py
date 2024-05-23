e3dfde2a3c719d184ad50037fcfed5a407f3316c
beaver.zip <95961223+beaver-zip@users.noreply.github.com>
2024-05-23 14:01:32 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,35 +1,21 @@
-import sys
-input = sys.stdin.readline
+# 출처: https://yuna0125.tistory.com/3
+import sys 
+n = int(sys.stdin.readline())
 
-n = int(input())
-stack = [None] * n
-top = -1
+stack = []
 
 for _ in range(n):
-    s = input().strip()
-    if s[:4] == 'push':
-        top += 1
-        stack[top] = int(s[5:])
-    elif s == 'pop':
-        if top == -1:
-            print(-1)
-        else:
-            data = stack[top]
-            stack[top] = None
-            top -= 1
-            print(data)
-    elif s == 'size':
-        if top == -1:
-            print(0)
-        else:
-            print(top + 1)
-    elif s == 'empty':
-        if top == -1:
-            print(1)
-        else:
-            print(0)
-    else:
-        if top == -1:
-            print(-1)
-        else:
-            print(stack[top])
+    s = sys.stdin.readline().split() 
+    if s[0] == 'push':
+        stack.append(s[1])
+    elif s[0] == 'pop':
+        if len(stack)>0: print(stack.pop())
+        else: print(-1)
+    elif s[0] == 'size':
+        print(len(stack))
+    elif s[0] == 'empty':
+        if len(stack)==0: print(1)
+        else: print(0)
+    elif s[0] == 'top':
+        if len(stack)>0: print(stack[-1])
+        else: print(-1)
