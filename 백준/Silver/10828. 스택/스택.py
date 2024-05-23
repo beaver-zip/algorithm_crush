338ad26573b10736889fd2410ab7aa238027053a
beaver.zip <95961223+beaver-zip@users.noreply.github.com>
2024-05-23 13:50:58 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,12 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-def isEmpty():
-    global stack, top
-    if top == -1:
-        return True
-    return False
-
 n = int(input())
 stack = [None] * n
 top = -1
@@ -17,7 +11,7 @@ for _ in range(n):
         top += 1
         stack[top] = int(s[5:])
     elif s == 'pop':
-        if isEmpty():
+        if top == -1:
             print(-1)
         else:
             data = stack[top]
@@ -25,17 +19,17 @@ for _ in range(n):
             top -= 1
             print(data)
     elif s == 'size':
-        if isEmpty():
+        if top == -1:
             print(0)
         else:
-            print(top+1)
+            print(top + 1)
     elif s == 'empty':
-        if isEmpty():
+        if top == -1:
             print(1)
         else:
             print(0)
     else:
-        if isEmpty():
+        if top == -1:
             print(-1)
         else:
             print(stack[top])
