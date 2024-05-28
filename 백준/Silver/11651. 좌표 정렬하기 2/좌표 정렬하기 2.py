92b69a5721de21d0d1e8e71232ed00c710630979
beaver.zip <95961223+beaver-zip@users.noreply.github.com>
2024-05-29 00:06:18 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 320 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -3,11 +3,9 @@ input = sys.stdin.readline
 
 arr = []
 for i in range(int(input())):
-    x, y = map(int, input().rstrip().split())
+    x, y = map(int, input().split())
     arr.append((x, y))
 
-arr.sort(key = lambda a: a[0])
-arr.sort(key = lambda a: a[1])
-
+arr.sort(key = lambda a: (a[1], a[0]))
 for a in arr:
     print(*a)
