6834a0a8cf9a0878b79d0d68ab574db58c5b0aa8
beaver.zip <95961223+beaver-zip@users.noreply.github.com>
2024-05-31 04:09:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.14 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -3,11 +3,10 @@ def solution(participant, completion):
     
     for p in participant:
         dict[p] += 1
-    
     for c in completion:
         dict[c] -= 1
         if dict[c] == 0:
             del dict[c]
-    
-    for key in dict.keys():
-        return key
+    # for key in dict.keys():
+    #     return key
+    return list(dict.keys())[0]
